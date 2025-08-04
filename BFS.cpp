28aa082f772cc8a24a3ca104240f87476fc8d601
RezwanaRifah <118298262+RezwanaRifah@users.noreply.github.com>
2025-08-04 19:06:22 +0000

    bfs code

---
+++ BFS.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, m; cin >> n >> m;
+	
+	vector<int> adj[n+1];
+	
+	for(int i = 0; i < m; i++){
+		int u, v; cin >> u >> v ;
+		
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	
+	int src = 1;
+	queue<int> q;
+	int level[n+1];
+	int visited[n+1] = {0};
+	
+	level[src] = 0;
+	q.push(src);
+	visited[src] = 1;
+	
+	while(!q.empty()){
+		int u = q.front();
+		q.pop();
+		
+		for(auto v: adj[u]){
+			if(visited[v]) continue;
+			
+			q.push(v);
+			level[v] = level[u] +1;
+			visited[v] = 1;
+		}		
+	}
+	
+	for(int i = 1; i <= n; i ++){
+		cout << i << "-->" << level[i] << endl;
+	}
+	
+	
+	
+	return 0;
+}
