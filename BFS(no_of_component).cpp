5b39c42a7e83f83b4469aff906e91c3a1faa6b37
RezwanaRifah <118298262+RezwanaRifah@users.noreply.github.com>
2025-08-06 18:10:46 +0000

    the number of component in a graph using bfs

---
+++ BFS(no_of_component).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, m; cin >> n >> m;
+	
+	vector<int> adj[n+1];
+	
+	for(int i = 0; i < m; i++){
+		int u, v; cin >> u >> v;
+		
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	
+	vector<int> visited(n+1 , 0);
+	
+	int count = 0;
+	
+	for(int i = 1; i<= n; i++){
+		if(visited[i]) continue;
+		
+		count++;
+		
+		queue<int> q;
+		q.push(i);
+		visited[i] = 1;
+		
+		while(!q.empty()){
+			int u = q.front();
+			q.pop();
+			for(auto v: adj[u]){
+				if(visited[v]) continue;
+				
+				q.push(v);
+				visited[v] = 1;
+			}
+		}
+	}
+	
+	cout << count << endl;
+	
+	return 0;
+}
